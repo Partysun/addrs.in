872318ac4617c5db82d35cdf67bcfe21e2c859a5
Yura Zatsepin <yura.zatsepin@gmail.com>
2021-11-11 19:13:57 +0300

    POC of balance checker

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-const express = require("express");
-const path = require("path");
+import express from 'express'
+import path from 'path'
+import fetch from 'node-fetch'
 
-const app = express();
-const port = process.env.PORT || "8000";
+const app = express()
+const port = process.env.PORT || "8000"
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
-});
+const slug = "ethereum"
+const url = "https://api.santiment.net/graphql"
+
+const query = (address) => JSON.stringify({query: `
+  {
+    blockchainAddress(selector: {address: "${address}", infrastructure: "ETH"}) {
+      balance(selector: {slug: "ethereum"})
+  }
+}
+`})
+
+app.get("/:address", async (req, res) => {
+  const address = req.params.address 
+  const response = await fetch(
+    url,
+    {
+      method: 'post',
+      body: query(address),
+      headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': 'Node',
+      },
+    }
+  )
+  const json = await response.json()
+  const balance = json.data?.blockchainAddress?.balance
+  res.status(200).send(`${address}: ${balance} ETH`)
+})
 
 app.listen(port, () => {
-  console.log(`Listening to requests on http://localhost:${port}`);
-});
+  console.log(`Listening to requests on http://localhost:${port}`)
+})
